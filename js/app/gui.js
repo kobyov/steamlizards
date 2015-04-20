f5e6ebcc5eddf9bf9243763f19579631fb67139c
kobyov <kobyov@gmail.com>
2015-04-20 18:13:19 +1200

    updated version number

--- js/app/gui.js
+++ js/app/gui.js
@@ -91,7 +91,7 @@ function initialise_gui() {
     $('#mainwindow').w2layout({
         name: 'mainwindow',
         panels: [
-            { type: 'top', size: 30, resizable: true, style: pstyle, content: 'Steam Lizards v0.06' },
+            { type: 'top', size: 30, resizable: true, style: pstyle, content: 'Steam Lizards v0.07' },
             { type: 'left', size: 200, resizable: true, style: pstyle, content: '<div id="storage"></div>' },
             { type: 'main', style: pstyle + 'border-top: 0px;', content: control_divs,
                 tabs: {
